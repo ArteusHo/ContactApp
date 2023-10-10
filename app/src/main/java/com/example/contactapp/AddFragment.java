2ccfa1643fedec9835e5d76702ed7528f691ec15
alexc <alexchan0724@gmail.com>
2023-10-11 00:22:10 +0800

    bruh

--- app/src/main/java/com/example/contactapp/AddFragment.java
+++ app/src/main/java/com/example/contactapp/AddFragment.java
@@ -1,53 +1,47 @@
 package com.example.contactapp;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AddFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AddFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private static final int REQUEST_CODE = 22;
 
-    // TODO: Rename and change types of parameters
+    ImageButton btnProfile;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    private String currentPhotoPath;
+
     private String mParam1;
     private String mParam2;
 
-    public AddFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddFragment newInstance(String param1, String param2) {
-        AddFragment fragment = new AddFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    private static final int REQUEST_CAMERA_PERMISSION_CODE = 1;
+    private static final int REQUEST_IMAGE_CAPTRURE = 2;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,13 +56,41 @@ public class AddFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_add, container, false);
         MenuData menuDataViewModel = new ViewModelProvider(getActivity()).get(MenuData.class);
+        Button btnBack = view.findViewById(R.id.btnBack);
+        btnProfile = view.findViewById(R.id.profile);
+        EditText firstname = view.findViewById(R.id.firstname);
 
-        Button btnBack=view.findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {menuDataViewModel.setOption(1);}
+            public void onClick(View view) {
+                // Handle the back button click if needed
+            }
+        });
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            Bundle extras = result.getData().getExtras();
+                            if (extras != null) {
+                                Bitmap photo = (Bitmap) extras.get("data");
+                                btnProfile.setImageBitmap(photo);
+                            }
+                        }
+                    }
+                });
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityResultLauncher.launch(camintent);
+            }
         });
 
         return view;
     }
+
+
 }
