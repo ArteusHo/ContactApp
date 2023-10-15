6a0fffaafd48e47d903dd7ffba010d7620becec7
Arttavich Ho <art.rangerx@gmail.com>
2023-10-15 23:49:11 +0800

    final

--- app/src/main/java/com/example/contactapp/PersonDatabase.java
+++ app/src/main/java/com/example/contactapp/PersonDatabase.java
@@ -3,7 +3,7 @@ package com.example.contactapp;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {PersonClass.class}, version = 1)
+@Database(entities = {PersonClass.class}, version = 1,exportSchema = false)
 public abstract class PersonDatabase extends RoomDatabase{
     public abstract PersonDAO personDAO();
 }
